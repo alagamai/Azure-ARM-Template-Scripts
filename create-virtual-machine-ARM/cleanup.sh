691d7fe98bef6d58d0f7fc88340d7fc7512fca77
Alagamai <alagamai.n@gmail.com>
2024-02-19 12:45:17 +0530

    update cleanup.sh

--- create-virtual-machine-ARM/cleanup.sh
+++ create-virtual-machine-ARM/cleanup.sh
@@ -1,6 +1,6 @@
 # Set the name of the resource group
 resourceGroup="vm-arm-test_group"
 
-az group delete --name resourceGroup --yes --no-wait
+az group delete --name $resourceGroup --yes --no-wait
 
 
