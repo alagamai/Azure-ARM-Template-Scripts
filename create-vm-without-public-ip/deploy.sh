7bbb0c235da335b565a5290650dc90442f1fd7e6
Alagamai <alagamai.n@gmail.com>
2024-02-20 09:05:38 +0530

    create arm template for vm with provate ip

---
+++ create-vm-without-public-ip/deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+az group create --name vm-arm-test_group --location eastus
+
+# Set the path to your template file
+templateFile="./template.json"
+# Set the name of the resource group
+resourceGroup="vm-arm-test_group"
+
+# Set the path to your parameters file
+parametersFile="./parameters.json"
+
+
+# Run the Azure CLI deployment command
+az deployment group create --name VM-template \
+                           --resource-group $resourceGroup \
+                           --template-file $templateFile \
+                           --parameters @$parametersFile
+
+
+
